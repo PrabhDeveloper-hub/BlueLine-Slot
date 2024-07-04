c40d7c96c6c8c1dff2e62f2850628e4309775908
PrabhjotSeedling <150044769+PrabhjotSeedling@users.noreply.github.com>
2024-07-04 23:13:54 +0530

    Symbol animation

--- src/app/Win/winAnimation.ts
+++ src/app/Win/winAnimation.ts
@@ -1,7 +1,7 @@
 import * as PIXI from "pixi.js";
 //@ts-ignore
 import TWEEN from "@tweenjs/tween.js";
-import { PAYLINES } from "../../cfg/game-variable-constants";
+import {  PAYLINES } from "../../cfg/game-variable-constants";
 
 export class WinAnimation {
 
@@ -9,7 +9,7 @@ export class WinAnimation {
     public graphics: PIXI.Graphics;
     public inProgress: Boolean = false;
     public spinEvent: Event;
-
+    public totalWinLines: Number = 0;
     constructor(texture: any, x: number, y: number) {
         this.winImg = new PIXI.Sprite(texture);
         this.winImg.anchor.set(0.5);
@@ -22,45 +22,24 @@ export class WinAnimation {
     }
 
     playWin() {
+        this.inProgress = true;
         new TWEEN.Tween(this.winImg.scale)
             .to({ x: 1, y: 1 }, 700).easing(TWEEN.Easing.Elastic.Out).onComplete(() => {
                 new TWEEN.Tween(this.winImg.scale)
                     .to({ x: 0, y: 0 }, 500).onComplete(() => {
                         this.graphics.clear();
-                        document.dispatchEvent(this.spinEvent);
+                        // document.dispatchEvent(this.spinEvent);
                     }).start()
             }).start()
 
     }
 
-    // createPaylines(winningSymbols: any) {
-    //     this.inProgress = true;
-    //     this.graphics.x = PAYLINECONFIG.x;
-    //     this.graphics.y = PAYLINECONFIG.y;
-    //     this.graphics.scale.x = 0.7;
-    //     this.graphics.lineStyle(10, 0x1434A4, 1);
-    //     this.graphics.moveTo(winningSymbols[0].x, winningSymbols[0].y * 0.5);
-    //     console.log(winningSymbols)
-    //     for (var points = 1; points < winningSymbols.length; points++) {
-    //         this.graphics.lineTo(winningSymbols[points].x, winningSymbols[points].y * 0.5);
-    //     }
-    //     this.graphics.endFill();
-    //     // var self = this;
-
-    //     // setTimeout(function () {
-    //     //     self.playWin();
-    //     // }, 1000);
-    // }
-
-
     toggleSymbols(winningSymbols: any) {
         for (let i = 0; i < winningSymbols.length; i++) {
-            new TWEEN.Tween(winningSymbols[i].symbol.scale)
-                .to({ x: 0.1, y: 0.1, }, 500).onComplete(() => {
-                    new TWEEN.Tween(winningSymbols[i].symbol.scale)
-                        .to({ x: 1, y: 1 }, 500).onComplete(() => {
-                            this.inProgress = false;
-                            // document.dispatchEvent(this.spinEvent);
+            new TWEEN.Tween(winningSymbols[i].scale)
+                .to({ x: 0.1, y: 0.1, }, 300).onComplete(() => {
+                    new TWEEN.Tween(winningSymbols[i].scale)
+                        .to({ x: 1, y: 1 }, 300).onComplete(() => {
                         }).start()
                 }).start()
         }
@@ -88,6 +67,7 @@ export class WinAnimation {
             }
         }
         if (allWinlines.length) {
+            this.totalWinLines = allWinlines.length;
             this.animateWinLines(allWinlines)
             this.playWin();
         } else {
@@ -96,6 +76,16 @@ export class WinAnimation {
     }
 
     animateWinLines(allWinlines: any) {
-        console.log(allWinlines)
+        for (let winline = 0; winline < allWinlines.length; winline++) {
+            setTimeout(() => {
+                if (this.totalWinLines == (winline + 1)) {
+                    setTimeout(() => {
+                        this.inProgress = false;
+                        document.dispatchEvent(this.spinEvent);
+                    }, 600);
+                }
+                this.toggleSymbols(allWinlines[winline]);
+            }, (winline + 2) * 600);
+        }
     }
 }
--- src/cfg/game-variable-constants.ts
+++ src/cfg/game-variable-constants.ts
@@ -1,42 +1,43 @@
 export const SYMBOL_IDS = [
-    "H1","H2","H3","H4",
+    "H1", "H2", "H3", "H4",
 ];
 export const STAGECONFIG = {
-    gameWidth:800,
-    gameHeight:600
+    gameWidth: 800,
+    gameHeight: 600
 }
 export const REELSCONFIG = {
-    SYMBOL_SIZE:160,
+    SYMBOL_SIZE: 160,
     REELS_COUNT: 3,
-    SYMBOL_PER_REEL:3,
-    REEL_WIDTH:160,
-    REEL_OFFSET:140,
-    SPIN_SPEED:70,
-    STOP_TIME:700,
+    SYMBOL_PER_REEL: 3,
+    REEL_WIDTH: 160,
+    REEL_OFFSET: 140,
+    SPIN_SPEED: 70,
+    STOP_TIME: 700,
 };
 export const PAYLINECONFIG = {
     x: -140,
-    y: 0   
+    y: 0
 }
 export const PAYLINES = [
-    [1,1,1],
-[1,1,2],
-[1,2,1],
-[1,2,2],
-[1,2,3],
-[2,1,1],
-[2,1,2],
-[2,2,1],
-[2,2,2],
-[2,2,3],
-[2,3,2],
-[3,2,1],
-[3,2,2],
-[3,2,3],
-[3,3,3],
-[3,3,1]
+    [1, 1, 1],
+    [1, 1, 2],
+    [1, 2, 1],
+    [1, 2, 2],
+    [1, 2, 3],
+    [2, 1, 1],
+    [2, 1, 2],
+    [2, 2, 1],
+    [2, 2, 2],
+    [2, 2, 3],
+    [2, 3, 2],
+    [2, 3, 3],
+    [3, 2, 1],
+    [3, 2, 2],
+    [3, 2, 3],
+    [3, 3, 3],
+    [3, 3, 2]
 ];
 
-export const DUMMY_WIN =  [
-    [['H2','H1','H2'],['H2','H2','H3'],['H1','H3','H2']]
+export const DUMMY_WIN = [
+    [['H2', 'H1', 'H2'], ['H2', 'H2', 'H3'], ['H1', 'H3', 'H2']]
 ]
